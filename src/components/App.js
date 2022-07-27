6fb0f59e79aa35da5e12a9fffaf6d3c8ef821b92
Nikhila Balmoori <nbalmoori@gmail.com>
2022-07-26 23:03:11 -0600

    Connect API to application

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import '../styling/App.css';
 import ArticleList from './ArticleList';
+import { useEffect, useState } from 'react';
+import getFetch from '../apiCalls';
 
 const App = () => {
+
+  const [displayedArticles, setDisplayedArticles] = useState([]);
+
+  useEffect(() => {
+    getFetch('home')
+    .then(data => setDisplayedArticles(data.results))
+  })
+
   return (
     <main>
     <header>NYTimes Top Stories</header>
