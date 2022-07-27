d8283c6b5f100496afa44687c6c1e8673ac2c7fd
Nikhila Balmoori <nbalmoori@gmail.com>
2022-07-27 10:24:07 -0600

    Fix bug for no thumbnail breaking app

--- src/components/ArticleLink.js
+++ src/components/ArticleLink.js
@@ -2,13 +2,22 @@ import '../styling/ArticleLink.css';
 
 const ArticleLink = ( {articleInfo} ) => {
 
-  const thumbnail = articleInfo.multimedia.find(media => media.format === 'Large Thumbnail')
+  const thumbnail = articleInfo.multimedia?.find(media => media.format === 'Large Thumbnail')
 
-  return (
-  <div className='articleLink'>
-    <img src={thumbnail.url} alt={thumbnail.caption}/>
-    <p className='articleLinkTitle'>{articleInfo.title}</p>
-  </div>
-)};
+  if (!thumbnail) {
+    return (
+      <div className='articleLink'>
+      <p className='articleLinkTitle'>{articleInfo.title}</p>
+    </div>
+    )
+  } else {
+    return (
+      <div className='articleLink'>
+        <img src={thumbnail.url} alt={thumbnail.caption}/>
+        <p className='articleLinkTitle'>{articleInfo.title}</p>
+      </div>
+    )
+  }
+};
 
 export default ArticleLink;
